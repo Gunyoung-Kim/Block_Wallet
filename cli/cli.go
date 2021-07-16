c52dfc71a30c0cb28c752fab6486e8a3ee0add9d
Gunyoung-Kim <gun025bba@naver.com>
2021-07-16 21:09:16 +0900

    Commend Line Interface

---
+++ cli/cli.go
@@ -0,0 +1,39 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/Gunyoung-Kim/blockchain/explorer"
+	"github.com/Gunyoung-Kim/blockchain/rest"
+)
+
+func usage() {
+	fmt.Printf("Please use the following flags:\n\n")
+	fmt.Printf("-port: 	Set the port of the server\n")
+	fmt.Printf("-mode: 	Choose between 'html' and 'rest' or 'both'\n")
+	os.Exit(0)
+}
+
+//Start CLI
+func Start() {
+	port := flag.Int("port", 4000, "Set Port of this server ")
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest' or 'both'")
+
+	flag.Parse()
+
+	switch *mode {
+	case "html":
+		explorer.Start(*port)
+	case "rest":
+		rest.Start(*port)
+	case "both":
+		go rest.Start(*port)
+		explorer.Start(*port + 1)
+	default:
+		usage()
+	}
+
+	flag.Parse()
+}
--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
-import (
-	"github.com/Gunyoung-Kim/blockchain/explorer"
-	"github.com/Gunyoung-Kim/blockchain/rest"
-)
+import "github.com/Gunyoung-Kim/blockchain/cli"
 
 func main() {
-	go explorer.Start(3000)
-	rest.Start(4000)
+	cli.Start()
 }
